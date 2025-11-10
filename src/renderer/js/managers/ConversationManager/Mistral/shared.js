538016ab22e61de310183bd9a300fb18e0876a3c
Skye Wambua <swskye17@gmail.com>
2025-11-10 18:37:51 +0300

    Shared resource for modular mistral handlers
    Signed-off-by: Skye Wambua <swskye17@gmail.com>

---
+++ src/renderer/js/managers/ConversationManager/Mistral/shared.js
@@ -0,0 +1,55 @@
+import { Mistral } from '@mistralai/mistralai';
+import { CanvasUtil } from '../../Canvas/CanvasUtils';
+import { ChatUtil, ChatDisplay } from '../util';
+
+export const chatdisplay = new ChatDisplay()
+export const chatutil = new ChatUtil()
+export const canvasutil = new CanvasUtil()
+
+export let MISTRAL_API_KEY = null
+
+export const appIsDev = window.desk.api2.appIsDev
+
+StateManager.set('codeBuffer', {})
+
+StateManager.set('user_message_pid', null)
+StateManager.set('ai_message_pid', null)
+
+async function loadApiKey() {
+    const key = await window.desk.api2.getKeys('mistral');
+    MISTRAL_API_KEY = key.mistralKey; // Assign to global variable
+    //console.log("Loaded API:", MISTRAL_API_KEY)
+}
+
+await loadApiKey()
+export const Mistarlclient = new Mistral({ apiKey: MISTRAL_API_KEY });
+
+
+/*
+Object.defineProperty(window, "codeBuffer", {
+    get() {
+        return codeBuffer;
+    },
+    set(value) {
+        codeBuffer = value;
+    }
+});
+
+Object.defineProperty(window, "userMessage", {
+    get() {
+        return userMessage;
+    },
+    set(value) {
+        userMessage = value;
+    }
+});
+
+Object.defineProperty(window, "aiMessage", {
+    get() {
+        return aiMessage;
+    },
+    set(value) {
+        aiMessage = value;
+    }
+});
+*/

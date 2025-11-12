74f940cdd843d28f3bf4b32fa8b65cbf08a73374
Skye Wambua <swskye17@gmail.com>
2025-11-12 20:40:05 +0300

    Move animation component to it's own file
    Signed-off-by: Skye Wambua <swskye17@gmail.com>

--- src/react-app/components/ConversationRenderer/Renderer.jsx
+++ src/react-app/components/ConversationRenderer/Renderer.jsx
@@ -107,7 +107,7 @@ export const AiMessage = ({
                                         (isThinking || think_content) ? (
                                             <div className="think-section">
                                                 <div className="flex items-center justify-between">
-                                                    <strong className="leading-widest font-brand text-light text-blue-400 dark:text-blue-300">Thoughts:</strong>
+                                                    <strong className="leading-widest font-brand text-light text-blue-400 dark:text-blue-400">Thoughts:</strong>
                                                     <button
                                                         className="text-sm text-gray-600 dark:text-gray-200"
                                                         onClick={(e) => FoldThinking(e, fold_id)}
@@ -118,7 +118,7 @@ export const AiMessage = ({
                                                     </button>
                                                 </div>
                                                 <div id={fold_id} className="">
-                                                    <div id="think-content">
+                                                    <div id="think-content" className='text-gray-500 dark:text-gray-400'>
                                                         <CodeBlockRenderer htmlContent={processedThinkHtml} />
                                                     </div>
                                                 </div>
@@ -133,7 +133,7 @@ export const AiMessage = ({
                                     }
                                     {
                                         actual_response && think_content ?
-                                            <strong className="text-green-300 dark:text-green-400">Response:</strong>
+                                            <strong className="text-green-400 dark:text-green-400">Response:</strong>
                                             : ''
                                     }
                                     <div id={message_id} className={message_id}>
@@ -209,31 +209,6 @@ export const FileContainer = ({ filedata }) => {
         </div>
     );
 };
-
-export const LoadingAnimation = ({ }) => {
-    const loaderUUID = `loader_${Math.random().toString(30).substring(3, 9)}`;
-
-    return (
-        <div id={loaderUUID} className='absolute bottom-[10vh] -left-3 z-[51]'>
-            <div id="loader-parent">
-                <svg width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" className="transform scale-75">
-                    <circle cx="12" cy="24" r="4" className="fill-green-500">
-                        <animate attributeName="cy" values="24;10;24;38;24" keyTimes="0;0.2;0.5;0.8;1" dur="1s" repeatCount="indefinite" />
-                        <animate attributeName="fill-opacity" values="1;.2;1" keyTimes="0;0.5;1" dur="1s" repeatCount="indefinite" />
-                    </circle>
-                    <circle cx="24" cy="24" r="4" className="fill-blue-500">
-                        <animate attributeName="cy" values="24;10;24;38;24" keyTimes="0;0.2;0.5;0.8;1" dur="1s" repeatCount="indefinite" begin="-0.4s" />
-                        <animate attributeName="fill-opacity" values="1;.2;1" keyTimes="0;0.5;1" dur="1s" repeatCount="indefinite" begin="-0.4s" />
-                    </circle>
-                    <circle cx="36" cy="24" r="4" className="fill-yellow-500">
-                        <animate attributeName="cy" values="24;10;24;38;24" keyTimes="0;0.2;0.5;0.8;1" dur="1s" repeatCount="indefinite" begin="-0.8s" />
-                        <animate attributeName="fill-opacity" values="1;.2;1" keyTimes="0;0.5;1" dur="1s" repeatCount="indefinite" begin="-0.8s" />
-                    </circle>
-                </svg>
-            </div>
-        </div>
-    )
-}
 
 export const ExportMenu = ({ export_id, message_id }) => {
     return (

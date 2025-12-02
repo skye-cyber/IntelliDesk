d5fbfd22976dbe849915924d5ae3ef296ea5a663
Skye Wambua <swskye17@gmail.com>
2025-12-02 23:49:50 +0300

    syn main dev

--- src/renderer/js/managers/ConversationManager/Mistral/Textual.js
+++ src/renderer/js/managers/ConversationManager/Mistral/Textual.js
@@ -48,12 +48,12 @@ export async function MistraChat({ text, model_name = window.currentModel }) {
         HandleProcessingEventChanges('show')
         StateManager.set('processing', true);
 
-        const stream = generateTextChunks()/*
+        const stream = //generateTextChunks()
         await mistral.client.chat.stream({
             model: model_name,
             messages: window.desk.api.getHistory(true),
             max_tokens: 3000
-        });*/
+        });
 
 
         //const stream = generateTextChunks(text)

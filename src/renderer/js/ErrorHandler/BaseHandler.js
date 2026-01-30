6687277893b1d73ce06995e0029ac0c99ca3b3fd
Skye Wambua <swskye17@gmail.com>
2026-01-31 00:26:10 +0300

    Use BaseErrorHandler for handling erros and keychain
    - Rotates keychain incase of 401 authorization error

---
+++ src/renderer/js/ErrorHandler/BaseHandler.js
@@ -0,0 +1,52 @@
+import { HandleProcessingEventChanges } from "../Utils/chatUtils"
+import { handleDevErrors } from "./ErrorHandler"
+import errorHandler from "../../../react-app/components/ErrorHandler/ErrorHandler"
+import { StateManager } from "../managers/StatesManager"
+import { appIsDev } from "../managers/ConversationManager/Mistral/shared"
+import { staticPortalBridge, streamingPortalBridge } from "../PortalBridge"
+import { clientmanager } from "../managers/ConversationManager/Mistral/ClientManager"
+
+
+export async function BaseErrorHandler(error, ai_ms_pid, callback) {
+    HandleProcessingEventChanges("hide")
+    window.desk.api.popHistory("user")
+    const user_text = StateManager.get('userInputText')
+
+    staticPortalBridge.closeComponent(StateManager.get('user_message_portal'))
+    streamingPortalBridge.closeStreamingPortal(ai_ms_pid)
+    staticPortalBridge.closeComponent(StateManager.get('loader-element-id'))
+
+    // Rotate keys if error is key error ie rate limit
+    if (error.statusCode && error.statusCode === 401) {
+        const rotation_okay = await clientmanager.rotate_keychain()
+
+        if (typeof rotation_okay !== 'string') return callback(user_text)
+
+        error = {
+            name: 'KeyChainError',
+            origin: 'Key Chain Rotatio',
+            message: typeof (rotation_okay === 'tring') ? rotation_okay : "No valid key founf in keychain",
+            type: 'KeyChain Error',
+            errorType: 'RuntimeError',
+            stack: 'clientmanager.rotate_keychain\n at BaseErrorHandler',
+            statusCode: 541
+        }
+
+    }
+
+    const isDev = false//await appIsDev()
+    if (isDev) {
+        handleDevErrors(error, StateManager.get('user_message_portal'), StateManager.get('ai_message_portal'), user_text)
+    } else {
+        errorHandler.showError(
+            {
+                title: error?.name,
+                message: error.message || error,
+                retryCallback: callback,
+                callbackArgs: {
+                    text: user_text,
+                    model_name: StateManager.get('currentModel')
+                }
+            })
+    }
+}
